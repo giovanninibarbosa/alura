a9a13dcfc7ba8c55f86e445c73ebbbba23a2e791
Giovannini Barbosa <giovanninibaarbosa@gmail.com>
2022-08-30 00:43:13 -0300

    feat: implements room implementation to DAO.

--- android/arquitetura-android/architecture-components/app/src/main/java/br/com/alura/technews/ui/viewmodel/factory/FormularioNoticiaViewModelFactory.kt
+++ android/arquitetura-android/architecture-components/app/src/main/java/br/com/alura/technews/ui/viewmodel/factory/FormularioNoticiaViewModelFactory.kt
@@ -3,13 +3,13 @@ package br.com.alura.technews.ui.viewmodel.factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import br.com.alura.technews.repository.NoticiaRepository
-import br.com.alura.technews.ui.viewmodel.ListaNoticiasViewModel
+import br.com.alura.technews.ui.viewmodel.FormularioNoticiaViewModel
 
-class ListaNoticiasViewModelFactory(
+class FormularioNoticiaViewModelFactory(
     private val repository: NoticiaRepository
 ) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return ListaNoticiasViewModel(repository) as T
+        return FormularioNoticiaViewModel(repository) as T
     }
 }
