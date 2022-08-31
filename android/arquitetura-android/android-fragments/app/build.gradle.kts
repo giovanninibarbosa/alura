078a7cc30aa07c0358c3d2db27950a6f9e27fded
Giovannini Barbosa <giovanninibaarbosa@gmail.com>
2022-08-31 15:44:58 -0300

    build: bump versions.

--- android/arquitetura-android/android-fragments/app/build.gradle.kts
+++ android/arquitetura-android/android-fragments/app/build.gradle.kts
@@ -6,14 +6,15 @@ plugins {
 }
 
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(33)
     defaultConfig {
         applicationId = "br.com.alura.technews"
         minSdkVersion(19)
-        targetSdkVersion(29)
+        targetSdkVersion(33)
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
     }
     buildTypes {
         getByName("release") {
@@ -35,7 +36,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.5.0")
     implementation("androidx.core:core-ktx:1.8.0")
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.0")
 
     implementation("junit:junit:4.13.2")
     implementation("androidx.test.ext:junit:1.1.3")
@@ -44,7 +45,7 @@ dependencies {
     implementation("androidx.test.espresso:espresso-core:3.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation("androidx.recyclerview:recyclerview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("com.google.android.material:material:1.6.1")
 
     // Https: Connection - Retrofit
