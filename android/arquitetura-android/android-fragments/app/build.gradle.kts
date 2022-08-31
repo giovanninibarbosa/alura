2fa0b63de89a6c62ffd5bee840487ae4fb3725f2
Giovannini Barbosa <giovanninibaarbosa@gmail.com>
2022-08-30 22:08:00 -0300

    build: bump versions

--- android/arquitetura-android/android-fragments/app/build.gradle.kts
+++ android/arquitetura-android/android-fragments/app/build.gradle.kts
@@ -31,28 +31,28 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
 
     implementation("androidx.room:room-runtime:2.4.3")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
-    implementation("androidx.appcompat:appcompat:1.0.2")
-    implementation("androidx.core:core-ktx:1.0.2")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.core:core-ktx:1.8.0")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
 
-    implementation("junit:junit:4.12")
-    implementation("androidx.test.ext:junit:1.1.0")
+    implementation("junit:junit:4.13.2")
+    implementation("androidx.test.ext:junit:1.1.3")
 
 
-    implementation("androidx.test.espresso:espresso-core:3.1.1")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.test.espresso:espresso-core:3.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     implementation("androidx.recyclerview:recyclerview:1.0.0")
-    implementation("com.google.android.material:material:1.0.0")
+    implementation("com.google.android.material:material:1.6.1")
 
     // Https: Connection - Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.5.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Https: Log Interceptor
-    implementation("com.squareup.okhttp3:logging-interceptor:3.14.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.8.1")
 
     // DI : Koin
     implementation("org.koin:koin-android:2.0.0-rc-2")
